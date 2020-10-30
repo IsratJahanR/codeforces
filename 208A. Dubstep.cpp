7a9db0f36861b88fe14b13cb18d9df79d6b8249c
Israt Jahan <65416257+IsratJahanR@users.noreply.github.com>
2020-10-31 00:23:56 +0600

    Add files via upload

--- 208A. Dubstep.cpp
+++ 208A. Dubstep.cpp
@@ -1,21 +1,34 @@
 #include<bits/stdc++.h>
-using namespace std;
 #define pb push_back
-#define mp make_pair
+using namespace std;
 int main()
 {
-    int n,k;
-    cin>>n>>k;
-    int a[n];
-    vector<int >x;
-    for(int i=0;i<n;i++)cin>>a[i];
-    int sum=0;
+    string s;
+    cin>>s;
+    int n=s.size();
+    vector<char> ch;
+    vector<char >ss;
     for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n;j++)
-        {
-            if((a[i]+a[j])%k==0)sum++;
-        }
+        if(i<=n-3 && s[i]=='W' && s[i+1]=='U' && s[i+2]=='B'){ch.pb('*'); i+=2;}
+        else ch.pb(s[i]);
     }
-    cout<<sum<<endl;
+
+    int j=0;
+    for(int i=0;i<ch.size();i++)
+    {
+        if(ch[i]>='A' && ch[i]<='Z')
+        {
+            if(j>0)ss.pb('*');
+            ss.pb(ch[i]);
+            j=0 ;
+        }
+        else if(i>0) j++;
+
+    }
+    for(int i=0;i<ss.size();i++)
+    {
+       if(ss[i]>='A'&& ss[j]<='Z') cout<<ss[i];
+       else if(i>0) cout<<" ";
+    }
 }
